e50713e7a48dbbf512096f9f10e0a054b71e490b
elinapro <32786204+elinapro@users.noreply.github.com>
2025-01-27 13:30:43 -0800

    iteration 1

--- js/index.js
+++ js/index.js
@@ -43,41 +43,81 @@ for (i = 0; i < skills.length; i++) {
 
 // }
 
+// Creating the Message Form
 
 const messageForm = document.getElementsByName("leave_message")[0];
 console.log(messageForm);
 // document.getElementById("submit_btn").addEventListener("click", myFunction);
 messageForm.addEventListener("submit", (event) => {
-    event.preventDefault(); // Prevent form submission
-    let name = event.target.usersName.value;
-    let email = event.target.usersEmail.value;
-    let message = event.target.usersMessage.value;
-    console.log(name, email, message);
-    messageForm.reset();
+  event.preventDefault(); // Prevent form submission
+  let name = event.target.usersName.value;
+  let email = event.target.usersEmail.value;
+  let message = event.target.usersMessage.value;
+  console.log(name, email, message);
+  messageForm.reset();
 
+  let messageSection = document.getElementById("messages");
+  console.log(messageSection);
+  let messageList = messageSection.getElementsByTagName("ul")[0];
+  console.log(messageList);
+  messageSection.hidden = true;
+  // document.getElementById("leave_message").requestFullscreen();
 
-    let messageSection = document.getElementById("messages");
-      console.log(messageSection)
-    let messageList = messageSection.getElementsByTagName("ul")[0];
-      console.log(messageList);
-      messageSection.hidden = true;
-    // document.getElementById("leave_message").requestFullscreen();
+  // Creating a new message
+  let newMessage = document.createElement("li");
+  newMessage.innerHTML = `<a href= 'mailto: ${email} '>${name} </a> <br /> <span>${message} </span>`;
 
-    let newMessage = document.createElement("li");
-    newMessage.innerHTML = `<a href= 'mailto: ${email} '>${name} </a> <br /> <span>${message} </span>`
+  // Adding the remove button
+  let removeButton = document.createElement("button");
+  removeButton.innerText = "Remove";
+  removeButton.type = "button";
+  removeButton.addEventListener("click", (event) => {
+    let entry = event.target.parentNode;
+    entry.remove();
+  });
 
-    let removeButton = document.createElement("button");
-    removeButton.innerText= "Remove";
-    removeButton.type = "button";
-    removeButton.addEventListener("click", (event) => {
-        let entry = event.target.parentNode;
-        entry.remove();
-    })
-
-    newMessage.appendChild(removeButton);
-    newMessage.appendChild(document.createElement("hr"))
-    messageList.appendChild(newMessage);
-    messageForm.reset();
-    messageSection.hidden = false;
+  // adding a message to the list along with a remove button and a name
+  newMessage.appendChild(removeButton);
+  newMessage.appendChild(document.createElement("hr"));
+  messageList.appendChild(newMessage);
+  messageForm.reset();
+  messageSection.hidden = false;
 });
+
+// Lesson -13 fetch
+
+//projects list
+
+const projectSection = document.getElementById("projects");
+console.log("projectSection: ", projectSection);
+
+const projectsList = projectSection.querySelector("ul");
+
+
+
+
+//Fetching the Github projects from the API
+
+fetch("https://api.github.com/users/elinapro/repos")
+  .then((response) => {
+    return response.json(); //the response from Github
+  })
+  .then((repositories) => {
+    console.log("repositories: ", repositories) //pass in a string and then the variable itself
+// loop through the reposities array
+    for(let i= 0; i < repositories.length; i++){
+      //get specific project data
+      const project = repositories[i].name;
+      //create the DOM (HTML) elements
+      const li = document.createElement("li");
+      //put the data from the project into the DOM element(li)
+      li.innerText = project;
+      //<li>Jupiter</li>
+      //Any list items would be listed
+      projectsList.appendChild(li);
+    }
+  })
+  .catch((error) =>{
+    console.log(error); //add error message to DOM
+  });
 
